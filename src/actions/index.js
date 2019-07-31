7a07071c9e429ac90a67b80f8bf446fd0854743b
Kelsy <kelsywells@gmail.com>
2019-07-30 17:22:14 -0700

    created getPeople function, added GET_PEOPLE_START, GET_PEOPLE_SUCCESS, GET_PEOPLE_FAIL and axios request in actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
+import { axios } from 'axios';
+export const FETCH_PEOPLE_SUCCESS = "FETCH_PEOPE_SUCCESS";
+export const FETCH_PEOPLE_FAIL = "FETCH_PEOPLE_FAIL";
+export const FETCH_PEOPLE_START = "FETCH_PEOPLE_START";
+
 // we'll need axios
 
 // we'll need to create 3 different action types here.
@@ -6,3 +11,21 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+export const getPeople = () => {
+    return function(dispatch) {
+        dispatch({
+            type: FETCH_PEOPLE_START
+        })
+        axios.get("https://swapi.co/api/people/")
+            .then(res => dispatch({
+                type: FETCH_PEOPLE_SUCCESS,
+                payload: res.data.url
+            }))
+            .catch(err=> dispatch({
+                type: FETCH_PEOPLE_FAIL,
+                payload: err.stack
+            }))
+    }
+}
+
